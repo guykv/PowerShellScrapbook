88977b7ad1255a39e335582bf2ebffbdc76675f1
Guy Kvaernberg <me@guyk.no>
2014-05-27 21:17:59 +0200

    Added Update-Readme

---
+++ Update-Readme.ps1
@@ -0,0 +1,30 @@
+﻿<#
+    .Synopsis
+    Updates README.md
+    .Description
+    This script extracts 
+#>
+
+
+$CONSTANTS = Data {
+    @{
+        TopText = @"
+psfunc
+======
+
+My reusable PowerShell functions
+
+This is simply a repository of PowerShell functions that might sometime
+in the future become handy once more.
+"@
+
+        Filename = "README.md"
+    }
+}
+
+$text = $CONSTANTS.TopText
+
+foreach ($f in (Get-ChildItem -Path . -Filter "*.ps1"))
+{
+    $synopsis = Get-Help -Name $f.FullName -
+}
