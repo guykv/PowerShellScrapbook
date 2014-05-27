d167840c9142fe753dfc1f758bb5f7cb149b7f0e
Guy Kvaernberg <me@guyk.no>
2014-05-27 23:03:19 +0200

    First attempt at automated summary-generator

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-psfunc
-======
+PowerShellRecycleBin
+====================
 
-My reusable PowerShell functions
+This is simply a repository of PowerShell functions and scripts that might sometime
+in the future become handy once more.
 
-This is simply a repository of PowerShell functions that might sometime in the future become handy once more.
+Name                             | Synopsis                                                                                                 
+--------------------------------------------------------------------------------------------------------------------------------------------
+Scripts\New-ADGroupStructure.ps1 | Creates organizational units and groups in Active Directory according to a simple XML configuration file.
+Scripts\New-TestUser.ps1         |                                                                                                          
--- Update-Readme.ps1
+++ Update-Readme.ps1
@@ -2,29 +2,152 @@
     .Synopsis
     Updates README.md
     .Description
-    This script extracts 
+    This script extracts information from the 
 #>
 
 
 $CONSTANTS = Data {
     @{
         TopText = @"
-psfunc
-======
+PowerShellRecycleBin
+====================
 
-My reusable PowerShell functions
-
-This is simply a repository of PowerShell functions that might sometime
+This is simply a repository of PowerShell functions and scripts that might sometime
 in the future become handy once more.
+
 "@
 
-        Filename = "README.md"
+        FileName = "README.md"
+    }
+}
+
+Function Get-PowerShellHelpCommentBlock
+{
+    Param
+    (
+        [Parameter()]
+        [string]$Path,
+
+        [Parameter()]
+        [string]$Keyword,
+
+        [Parameter()]
+        [switch]$KeepKeywordLine
+    )
+
+    $eating = $false
+    $capturedLines = @()
+    foreach ($line in (Get-Content -Path $Path))
+    {
+        if ($eating)
+        {
+            if ($line -match "^\s*\." -or $line -match "#>")
+            {
+                break
+            }
+
+            $capturedLines += $line
+        }
+
+        if ($line -match $Keyword)
+        {
+            $eating = $true
+            if ($KeepKeywordLine)
+            {
+                $capturedLines += $line
+            }
+        }
+    }
+
+    $capturedLines
+}
+
+Function Format-MarkdownTable
+{
+    Param
+    (
+        [Parameter(ValueFromPipeline = $true)]
+        [PSObject]$InputObject,
+
+        [Parameter()]
+        [string[]]$Properties
+    )
+
+    Begin
+    {
+        $maxWidths = @()
+        $allRows = @()
+        if ($Properties)
+        {
+            $header = $Properties
+        }
+    }
+
+    Process
+    {
+        if (-not $header)
+        {
+            $header = @()
+            foreach ($p in Get-Member -InputObject $InputObject -MemberType Property,NoteProperty)
+            {
+                $header += $p.Name
+                $maxWidths += 0
+            }
+
+            $allRows += ,$header
+        }
+
+        $row = @()
+        for ($i = 0; $i -lt $header.Length; $i++)
+        {
+            [string]$value = $InputObject.$($header[$i])
+            $maxWidths[$i] = [Math]::Max($maxWidths[$i], $value.Length)
+            $row += $value
+        }
+
+        $allRows += ,$row
+    }
+
+    End
+    {
+        $firstRow = $true
+        foreach ($r in $allRows)
+        {
+            $cells = @()
+            for ($i = 0; $i -lt $header.Length; $i++)
+            {
+                if ($r[$i])
+                {
+                    $cells += $r[$i].PadRight($maxWidths[$i], " ")
+                }
+                else
+                {
+                    $cells += New-Object String(" ", $maxWidths[$i])
+                }
+            }
+
+            $row = $cells -join " | "
+            Write-Output -InputObject $row
+            if ($firstRow)
+            {
+                Write-Output -InputObject (New-Object String("-", $row.Length))
+                $firstRow = $false
+            }
+        }
     }
 }
 
 $text = $CONSTANTS.TopText
 
-foreach ($f in (Get-ChildItem -Path . -Filter "*.ps1"))
+$rows = @()
+foreach ($f in (Get-ChildItem -Path "$PSScriptRoot\Scripts" -Filter "*.ps1"))
 {
-    $synopsis = Get-Help -Name $f.FullName -
+    $synopsis = (Get-PowerShellHelpCommentBlock -Path $f.FullName -Keyword ".SYNOPSIS" | %{ $_.Trim() }) -join " "
+    $rows += New-Object -TypeName PSObject -Property @{
+        Name = "Scripts\" + $f.Name
+        Synopsis = $synopsis
+    }
 }
+
+$CONSTANTS.TopText | Set-Content -Path $CONSTANTS.FileName
+$rows | Format-MarkdownTable | Add-Content -Path $CONSTANTS.FileName
