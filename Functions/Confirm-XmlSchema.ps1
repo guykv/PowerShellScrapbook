f4cfa74c912468b366a4aeb16ef40e40a790c754
Guy Kvaernberg <me@guyk.no>
2014-05-28 10:59:17 +0200

    Converted back to function

--- Functions/Confirm-XmlSchema.ps1
+++ Functions/Confirm-XmlSchema.ps1
@@ -1,60 +1,63 @@
-<#
-    .Synopsis
-    Confirms that a given XML file conforms to the specified
-    XSD schema
-    .Description
-    This function uses the built in XML API to confirm that
-    XML files conforms to a specific XSD schema definition.
-    If the validation fails, an error is thrown.
-    .Parameter XsdPath
-    Path to the XSD schema file
-    .Parameter XmlPath
-    One or more paths to XML files to validate
-#>
-[CmdletBinding()]
-Param
-(
-    [Parameter(Mandatory=$true)]
-	[string]$XsdPath,
+Function Confirm-XmlSchema
+{
+    <#
+        .Synopsis
+        Confirms that a given XML file conforms to the specified
+        XSD schema
+        .Description
+        This function uses the built in XML API to confirm that
+        XML files conforms to a specific XSD schema definition.
+        If the validation fails, an error is thrown.
+        .Parameter XsdPath
+        Path to the XSD schema file
+        .Parameter XmlPath
+        One or more paths to XML files to validate
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+	    [string]$XsdPath,
 		
-    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-    [Alias('Path')]
-	[string[]]$XmlPath
-)
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Alias('Path')]
+	    [string[]]$XmlPath
+    )
     
-Begin
-{
-	$xsd = Get-Item -Path $XsdPath -ErrorAction Stop
-    $schemas = New-Object System.Xml.Schema.XmlSchemaSet
-    $xmlReader = New-Object System.Xml.XmlTextReader($xsd.FullName)
-    $schemas.Add($null, $xmlReader) | Out-Null
-    $xmlReader.Close()
-    $settings = New-Object System.Xml.XmlReaderSettings
-    $settings.ValidationType = [System.Xml.ValidationType]::Schema
-    $settings.Schemas = $schemas
-}
-
-Process
-{
-    foreach ($path in $XmlPath)
+    Begin
     {
-		$xml = Get-Item -Path $path
-        if (-not $xml)
-        {
-            continue
-        }
+	    $xsd = Get-Item -Path $XsdPath -ErrorAction Stop
+        $schemas = New-Object System.Xml.Schema.XmlSchemaSet
+        $xmlReader = New-Object System.Xml.XmlTextReader($xsd.FullName)
+        $schemas.Add($null, $xmlReader) | Out-Null
+        $xmlReader.Close()
+        $settings = New-Object System.Xml.XmlReaderSettings
+        $settings.ValidationType = [System.Xml.ValidationType]::Schema
+        $settings.Schemas = $schemas
+    }
 
-        $fs = [System.IO.File]::OpenRead($xml.FullName)
-        try
+    Process
+    {
+        foreach ($path in $XmlPath)
         {
-            $reader = [System.Xml.XmlReader]::Create((New-Object System.IO.StreamReader($fs)), $settings)
-            while ($reader.Read())
+		    $xml = Get-Item -Path $path
+            if (-not $xml)
             {
+                continue
             }
-        }
-        finally
-        {
-            $fs.Dispose()
+
+            $fs = [System.IO.File]::OpenRead($xml.FullName)
+            try
+            {
+                $reader = [System.Xml.XmlReader]::Create((New-Object System.IO.StreamReader($fs)), $settings)
+                while ($reader.Read())
+                {
+                }
+            }
+            finally
+            {
+                $fs.Dispose()
+            }
         }
     }
 }
