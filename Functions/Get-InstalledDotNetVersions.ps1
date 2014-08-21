f6c95b67145a16e712b74d277ef44ef9673d5a71
Guy Kvaernberg <me@guyk.no>
2014-08-21 14:30:10 +0200

    Added .NET detection script

---
+++ Functions/Get-InstalledDotNetVersions.ps1
@@ -0,0 +1,89 @@
+﻿Function Get-InstalledFrameworks
+{
+    <#
+        .SYNOPSIS
+        Detects which version(s) of .NET Framework that are present on this system.
+        .DESCRIPTION
+        Uses the methods described in http://support.microsoft.com/kb/315291 and
+        http://msdn.microsoft.com/en-us/library/hh925568(v=vs.110).aspx#net_b to determine
+        installed versions of .NET Framework.
+    #>
+    $oldPath = "HKLM:\Software\Microsoft\.NETFramework\Policy"
+    $path = "HKLM:\Software\Microsoft\Net Framework Setup\NDP"
+    @(
+        @{
+            Path = "$oldPath\v1.0"
+            Name = "3705"
+            Value = "3321-3705"
+            Version = "1.0"
+        },
+        @{
+            Path = "$oldPath\v1.1"
+            Name = "4322"
+            Value = "3706-4322"
+            Version = "1.1"
+        },
+        @{
+            Path = "$oldPath\v2.0"
+            Name = "50727"
+            Value = "50727-50727"
+            Version = "2.0"
+        },
+        @{
+            Path = "$path\v3.0\Setup"
+            Name = "InstallSuccess"
+            Value = "1"
+            Version = "3.0"
+        },
+        @{
+            Path = "$path\v3.5"
+            Name = "Install"
+            Value = "1"
+            Version = "3.5"
+        },
+        @{
+            Path = "$path\v4\Client"
+            Name = "Install"
+            Value = "1"
+            Version = "4.0c"
+        },
+        @{
+            Path = "$path\v4\Full"
+            Name = "Install"
+            Value = "1"
+            Version = "4.0"
+        },
+        @{
+            Path = "$path\v4\Full"
+            Name = "Release"
+            Value = "378389"
+            Version = "4.5"
+        },
+        @{
+            Path = "$path\v4\Full"
+            Name = "Release"
+            Value = "378675"
+            Version = "4.5.1"
+        },
+        @{
+            Path = "$path\v4\Full"
+            Name = "Release"
+            Value = "378758"
+            Version = "4.5.1"
+        },
+        @{
+            Path = "$path\v4\Full"
+            Name = "Release"
+            Value = "379893"
+            Version = "4.5.2"
+        }
+    ) | Where-Object -FilterScript {
+        $reg = Get-ItemProperty -Path $_.Path -Name $_.Name -ErrorAction SilentlyContinue
+        if ($reg)
+        {
+            $_.Value -eq $reg.$($_.Name)
+        }
+    } | ForEach-Object -Process {
+        $_.Version
+    }
+}
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ in the future become handy once more.
 Name                                    | Type     | Synopsis                                                                          
 --------------------------------------- | -------- | ----------------------------------------------------------------------------------
 Confirm-XmlSchema.ps1                   | Function | Confirms that a given XML file conforms to the specified XSD schema               
+Get-InstalledDotNetVersions.ps1         | Function | Detects which version(s) of .NET Framework that are present on this system.       
 New-ADGroupStructure.ps1                | Script   | Creates organizational units and groups in Active Directory from XML configuration
 New-TestUser.ps1                        | Script   | Creates any number of test users in Active Directory with common characteristics  
 Set-DefaultHomeDirectoryPermissions.ps1 | Script   | Sets default permissions and ownership to a user's home directory                 
